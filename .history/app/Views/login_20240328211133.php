2ae9ffebc95cf709e2a157e18169164cf009482e
Alexis Aguila <Alexisplus18@live.com>
2024-03-28 22:02:42 -0600

    Inicio de Proyecto
    - Se levanto el proyecto
    
    - Se ha creado el codigo SQL para levantar la base de datos.
    
    - Se creo un simple login (routes, views  etc.)

---
+++ .history/app/Views/login_20240328211133.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Login</title>
+      <link rel="stylesheet" href="<?php echo base_url('/backend/src/styles.login.css'); ?>">
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+   </head>
+   <body>
+      <div class="login-form">
+         <div class="text">
+            LOGIN
+         </div>
+         <form>
+            <div class="field">
+               <div class="fas fa-envelope"></div>
+               <input type="text" placeholder="ID de Usuario">
+            </div>
+            <div class="field">
+               <div class="fas fa-lock"></div>
+               <input type="password" placeholder="Password">
+            </div>
+            <button>LOGIN</button>
+         </form>
+      </div>
+   </body>
+</html>
